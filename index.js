17c33f92b6f5ebed03be16662b37925585762ce6
thaingocquang <thaingocquang147@gmail.com>
2021-12-28 22:30:49 +0700

    typing, chat,..

--- index.js
+++ index.js
@@ -2,21 +2,45 @@ import express from 'express'
 import bodyParser from 'body-parser'
 
 const app = express();
-app.use(express.static(__dirname + '/public'));
-const path = require('path')
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'))
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
+
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-
+const arrUser = []
 
 io.on("connection", (socket) => {
-    socket.on("client-send-data", (data) => {
-        console.log(data)
-        socket.broadcast.emit("server-send-data", data);
+    socket.on('client-send-register', (data) => {
+        console.log("Co nguoi vua ket noi: ", socket.id, ' - ', data);
+        if (arrUser.indexOf(data) >= 0) {
+            socket.emit('server-send-register-fail');
+        } else {
+            arrUser.push(data);
+            socket.Username = data;
+            socket.emit('server-send-register-success', data);
+            io.sockets.emit('server-send-arrUser', arrUser);
+        }
+    })
 
+    socket.on('logout', () => {
+        arrUser.splice(arrUser.indexOf(socket.Username), 1);
+        socket.broadcast.emit('server-send-arrUser', arrUser);
+    })
+
+    socket.on('user-send-message', (message) => {
+        io.sockets.emit('server-send-message', { username: socket.Username, message: message });
+    })
+
+    socket.on('typing', () => {
+        let s = socket.Username + ' is typing...'
+        // console.log(socket.Username + " dang go chu");
+        io.sockets.emit('someone-is-typing', s);
+    })
+
+    socket.on('stop-typing', () => {
+        io.sockets.emit('someone-stop-typing');
     })
 
 });
--- public/main.js
+++ public/main.js
@@ -1,3 +1,67 @@
 window.onload = () => {
     document.getElementById("home").style.display = "none";
 }
+
+document.getElementById('typing').style.display = "none";
+
+const socket = io('http://localhost:3000/');
+
+document.getElementById('btnRegister').onclick = () => {
+    let username = document.getElementById('txtUsername').value;
+    socket.emit('client-send-register', username);
+}
+
+socket.on('server-send-register-fail', () => {
+    alert("Username already existed!");
+})
+
+socket.on('server-send-register-success', (data) => {
+    document.getElementById('currentUser').innerHTML = data;
+    document.getElementById("home").style.display = "block";
+    document.getElementById("login").style.display = "none";
+})
+
+socket.on('server-send-arrUser', (arrUser) => {
+    document.getElementById('boxContent').innerText = "";
+    arrUser.forEach(element => {
+        let child = document.createElement('div');
+        child.innerText = element;
+        document.getElementById('boxContent').appendChild(child);
+    });
+
+})
+
+document.getElementById('btnLogout').onclick = () => {
+    socket.emit('logout');
+    document.getElementById("home").style.display = "none";
+    document.getElementById("login").style.display = "block";
+}
+
+document.getElementById('btnSendMessage').onclick = () => {
+    let message = document.getElementById("txtMessage").value;
+    socket.emit('user-send-message', message);
+}
+
+socket.on('server-send-message', (data) => {
+    let child = document.createElement('div');
+    child.innerText = data.username + ': ' + data.message;
+    document.getElementById('listMessage').appendChild(child);
+})
+
+document.getElementById('txtMessage').addEventListener('focusin', () => {
+    socket.emit('typing');
+})
+
+document.getElementById('txtMessage').addEventListener('focusout', () => {
+    socket.emit('stop-typing');
+})
+
+socket.on('someone-is-typing', (data) => {
+    document.getElementById('typing').style.display = "block";
+    document.getElementById('span').innerText = data;
+})
+
+socket.on('someone-stop-typing', () => {
+    document.getElementById('typing').style.display = "none";
+})
+
